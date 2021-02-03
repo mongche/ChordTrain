834d35d2c22a9ba585b06092e3a3771b14d872eb
mongche <mongche.lee@gmail.com>
2021-02-04 01:27:04 +0800

    Define the constants of chord label and music chords

---
+++ src/Constants/ChordLabel.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Chord\Constants;
+
+class ChordLabel
+{
+    /**
+     * The eazy label.
+     *
+     * @var string
+     */
+    const EAZY = 'eazy';
+
+    /**
+     * The medium label.
+     *
+     * @var string
+     */
+    const MEDIUM = 'medium';
+
+    /**
+     * The hard label.
+     *
+     * @var string
+     */
+    const HARD = 'hard';
+}
---
+++ src/Constants/SongChords.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Chord\Constants;
+
+class SongChords
+{
+    const IMAGINE = ['c', 'cmaj7', 'f', 'am', 'dm', 'g', 'e7'];
+
+    const SOMEWHERE_OVER_THE_RAINBOW = ['c', 'em', 'f', 'g', 'am'];
+
+    const TOO_MANY_COOKS = ['c', 'g', 'f'];
+
+    const I_WILL_FOLLOW_YOU_INTO_THE_DARK = ['f', 'dm', 'bb', 'c', 'a', 'bbm'];
+
+    const BABY_ONE_MORE_TIME = ['cm', 'g', 'bb', 'eb', 'fm', 'ab'];
+
+    const CREEP = ['g', 'gsus4', 'b', 'bsus4', 'c', 'cmsus4', 'cm6'];
+
+    const PAPER_BAG = ['bm7', 'e', 'c', 'g', 'b7', 'f', 'em', 'a', 'cmaj7', 'em7', 'a7', 'f7', 'b'];
+
+    const TOXIC = ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7', 'g7'];
+
+    const BULLET_PROOF = ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'];
+}
